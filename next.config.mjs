1f3839fe54df29f6f8a75fa7e8d516298994f463
Muhammad Nur Faza <gofar222@gmail.com>
2024-02-07 09:57:26 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// Configuration object tells the next-pwa plugin 
+const withPWA = withPWAInit({
+    dest: "public", // Destination directory for the PWA files
+    cacheOnFrontEndNav: true,
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+    reloadOnOnline: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true
+    }
+});
+
+export default withPWA(nextConfig);
